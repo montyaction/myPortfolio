a7b2cbf231ed417a39471eba4d95b37359ed5f21
montyaction <montyaction@hotmail.com>
2026-02-12 16:21:13 +0530

    chore: improve eslint and webpack config

--- eslint.config.js
+++ eslint.config.js
@@ -1,14 +1,24 @@
 const js = require("@eslint/js");
 const reactPlugin = require("eslint-plugin-react");
+const reactHooksPlugin = require("eslint-plugin-react-hooks");
+const importPlugin = require("eslint-plugin-import");
 const babelParser = require("@babel/eslint-parser");
 const globals = require("globals");
 
 module.exports = [
+  {
+    ignores: ["dist/**", "node_modules/**", "coverage/**"],
+    linterOptions: {
+      reportUnusedDisableDirectives: "error",
+    },
+  },
   js.configs.recommended,
   {
-    files: ["**/*.js", "**/*.jsx"],
+    files: ["src/**/*.{js,jsx}"],
     plugins: {
       react: reactPlugin,
+      "react-hooks": reactHooksPlugin,
+      import: importPlugin,
     },
     languageOptions: {
       parser: babelParser,
@@ -25,24 +35,61 @@ module.exports = [
       },
       globals: {
         ...globals.browser,
-        ...globals.jest,
-        ...globals.node,
+        process: "readonly",
       },
     },
     settings: {
       react: {
         version: "detect",
       },
+      "import/resolver": {
+        node: {
+          extensions: [".js", ".jsx"],
+        },
+      },
     },
     rules: {
       ...reactPlugin.configs.recommended.rules,
+      ...reactHooksPlugin.configs.recommended.rules,
+      ...importPlugin.configs.recommended.rules,
       "react/react-in-jsx-scope": "off",
       "react/prop-types": "off",
       "react/no-unescaped-entities": "off",
-      "no-unused-vars": "warn",
+      "no-unused-vars": ["warn", { argsIgnorePattern: "^_", ignoreRestSiblings: true }],
     },
   },
   {
-    ignores: ["dist/**", "node_modules/**"],
+    files: ["src/**/__tests__/**/*.{js,jsx}", "**/*.test.{js,jsx}", "**/*.spec.{js,jsx}"],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.jest,
+      },
+    },
+  },
+  {
+    files: ["__mocks__/**/*.js"],
+    languageOptions: {
+      sourceType: "script",
+      globals: {
+        ...globals.node,
+        ...globals.jest,
+      },
+    },
+  },
+  {
+    files: [
+      "webpack.config.js",
+      "jest.config.js",
+      "eslint.config.js",
+      "fileTransformer.js",
+      "*.config.js",
+    ],
+    languageOptions: {
+      sourceType: "script",
+      globals: {
+        ...globals.node,
+      },
+    },
   },
 ];
--- webpack.config.js
+++ webpack.config.js
@@ -3,93 +3,98 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ESLintPlugin = require('eslint-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
-module.exports = {
-  entry: {
-    bundle: path.resolve(__dirname, './src/index.js'),
-  },
+module.exports = (env, argv) => {
+  const isProd = argv.mode === 'production';
 
-  // This property defines the file path and the file name which will be used for deploying the bundled file
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: '[name][contenthash].js',
-    // publicPath: '/',
-    // assetModuleFilename: 'images/[name][hash][ext][query]',  // For handling images
-    clean: true,  // Clean the dist folder on each build
-  },
-
-  mode: 'development',
-
-  // Development server configuration
-  devServer: {
-    static: {
-      directory: path.join(__dirname, 'dist'),  // Path to serve static files
+  return {
+    entry: {
+      bundle: path.resolve(__dirname, './src/index.js'),
     },
-    compress: true,
-    port: 8080,
-    open: true,
-    hot: true,
-    historyApiFallback: true,
-  },
 
-  // Module rules and loaders
-  module: {
-    rules: [
-      // JavaScript and JSX loader
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env', '@babel/preset-react'],  // Presets for JS/JSX
-          }
-        },
-      },
-      // CSS loader
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
-      },
-      // Image loader
-      {
-        test: /\.(png|jpe?g|gif|svg)$/i,
-        type: 'asset/resource',
-        generator: {
-          filename: 'images/[name][hash][ext]',
-        },
-      },
-      // Font loader
-      {
-        test: /\.(woff|woff2|ttf|otf|eot)$/,
-        type: 'asset/resource',
-        generator: {
-          filename: 'fonts/[name][hash][ext]',
-        },
-      },
-    ],
-  },
+    // This property defines the file path and the file name which will be used for deploying the bundled file
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: isProd ? '[name][contenthash].js' : '[name].js',
+      // publicPath: '/',
+      // assetModuleFilename: 'images/[name][hash][ext][query]',  // For handling images
+      clean: true,  // Clean the dist folder on each build
+    },
 
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/index.html',
-    }),
-    new ESLintPlugin({
-      extensions: ['js', 'jsx'],
-      emitWarning: true,
-      emitError: false,
-      failOnError: false,
-      failOnWarning: false,
-      configType: 'flat',
-    }),
+    devtool: isProd ? 'source-map' : 'eval-cheap-module-source-map',
 
-    new CopyWebpackPlugin({
-      patterns: [
-        { from: 'src/service-worker.js', to: 'service-worker.js' },
+    // Development server configuration
+    devServer: {
+      static: {
+        directory: path.join(__dirname, 'dist'),  // Path to serve static files
+      },
+      compress: true,
+      port: 8080,
+      open: true,
+      hot: true,
+      historyApiFallback: true,
+    },
+
+    // Module rules and loaders
+    module: {
+      rules: [
+        // JavaScript and JSX loader
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env', '@babel/preset-react'],  // Presets for JS/JSX
+            }
+          },
+        },
+        // CSS loader
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader'],
+        },
+        // Image loader
+        {
+          test: /\.(png|jpe?g|gif|svg)$/i,
+          type: 'asset/resource',
+          generator: {
+            filename: 'images/[name][hash][ext]',
+          },
+        },
+        // Font loader
+        {
+          test: /\.(woff|woff2|ttf|otf|eot)$/,
+          type: 'asset/resource',
+          generator: {
+            filename: 'fonts/[name][hash][ext]',
+          },
+        },
       ],
-    }),
-  ],
+    },
 
-  resolve: {
-    extensions: ['.js', '.jsx', '.mjs'],
-  },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './src/index.html',
+        favicon: './src/assets/images/MA-logo_white.png',
+      }),
+      new ESLintPlugin({
+        extensions: ['js', 'jsx'],
+        emitWarning: true,
+        emitError: false,
+        failOnError: false,
+        failOnWarning: false,
+        configType: 'flat',
+      }),
+
+      new CopyWebpackPlugin({
+        patterns: [
+          { from: 'src/service-worker.js', to: 'service-worker.js' },
+        ],
+      }),
+    ],
+
+    resolve: {
+      extensions: ['.js', '.jsx', '.mjs'],
+    },
+  };
 };
